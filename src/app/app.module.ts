2975174fe1a4c5607f01ea846cced13c66e1b0f0
Maxim Dashkov <maxim_dashkov@epam.com>
2019-10-16 21:16:24 +0300

    add components to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { SearchComponent } from './search/search.component';
-import { CourseComponent } from './course/course.component';
-import { FooterComponent } from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {BreadcrumbsComponent} from './breadcrumbs/breadcrumbs.component';
+import {SearchComponent} from './search/search.component';
+import {CourseComponent} from './course/course.component';
+import {FooterComponent} from './footer/footer.component';
 
 @NgModule({
     declarations: [
