1d69a2ff830cb777b9a88fcf1c6f0a760f5a83b8
Maxim Dashkov <maxim_dashkov@epam.com>
2019-10-16 21:07:15 +0300

    add component blanks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { SearchComponent } from './search/search.component';
+import { CourseComponent } from './course/course.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        BreadcrumbsComponent,
+        SearchComponent,
+        CourseComponent,
+        FooterComponent
     ],
     imports: [
         BrowserModule
