2ec7f147f44d6327f7442924661faecc4ad29798
maxim dashkov <insektary@mail.ru>
2019-10-26 22:41:10 +0300

    complete layout

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -18,7 +18,7 @@ export class CourseListComponent implements OnInit {
                 creationDate: '12.12.2001',
                 duration: '60h 28m',
                 startDate: '9 Nov, 2019',
-                description: 'Бетономешалка мешает бетон, Бригада строителей жрет самогон. Прораб Петрович, в доску пьян, Спать устроился в мешалке, Электрик Степа пуск нажал На бетонной скороварке. Бригада строителей жрет самогон, В бетономешалке кровавый раствор. Тесто с мясом хорошо Здесь уже перемешалось Этим вкусным пирогом Вся бригада объедалась… Бетономешалка - алкоголизм Бетономешалка - каннибализм'
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
             },
             {
                 id: 2,
@@ -26,7 +26,39 @@ export class CourseListComponent implements OnInit {
                 creationDate: '12.12.2001',
                 duration: '60h 28m',
                 startDate: '9 Nov, 2019',
-                description: 'Бетономешалка мешает бетон, Бригада строителей жрет самогон. Прораб Петрович, в доску пьян, Спать устроился в мешалке, Электрик Степа пуск нажал На бетонной скороварке. Бригада строителей жрет самогон, В бетономешалке кровавый раствор. Тесто с мясом хорошо Здесь уже перемешалось Этим вкусным пирогом Вся бригада объедалась… Бетономешалка - алкоголизм Бетономешалка - каннибализм'
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },
+            {
+                id: 2,
+                title: 'Video course 1. Name tag',
+                creationDate: '12.12.2001',
+                duration: '60h 28m',
+                startDate: '9 Nov, 2019',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },
+            {
+                id: 3,
+                title: 'Video course 1. Name tag',
+                creationDate: '12.12.2001',
+                duration: '60h 28m',
+                startDate: '9 Nov, 2019',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },
+            {
+                id: 4,
+                title: 'Video course 1. Name tag',
+                creationDate: '12.12.2001',
+                duration: '60h 28m',
+                startDate: '9 Nov, 2019',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },
+            {
+                id: 5,
+                title: 'Video course 1. Name tag',
+                creationDate: '12.12.2001',
+                duration: '60h 28m',
+                startDate: '9 Nov, 2019',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
             }
         ];
     }
