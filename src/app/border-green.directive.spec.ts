48e9026b62a0cf9c4c7e7437347a3a5c79964102
Maxim_Dashkov <Maxim_Dashkov@epam.com>
2019-11-13 18:37:28 +0300

    add directive tests

--- src/app/border-green.directive.spec.ts
+++ src/app/border-green.directive.spec.ts
@@ -1,43 +1,152 @@
-import {Directive, ElementRef, Input, OnChanges} from '@angular/core';
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { Component, HostListener } from '@angular/core';
-import { BorderGreenDirective } from './border-green.directive';
+import {NO_ERRORS_SCHEMA, ElementRef, Input} from '@angular/core';
+import {TestBed, ComponentFixture} from '@angular/core/testing';
+import {Component} from '@angular/core';
+import {By} from '@angular/platform-browser';
+import {BorderGreenDirective} from './border-green.directive';
+
+const DATE = 'Mon Nov 18 2019 12:32:41 GMT+0300';
+const DATE2 = 'Fri Nov 1 2019 12:32:41 GMT+0300';
+
+@Component({
+    template: `<div appBorderGreen [startDate]='startDate'>Test</div>`
+})
+class AboutComponent {
+    public startDate: string;
+}
 
 describe('BorderGreenDirective', () => {
-
-    @Component({
-        // selector: 'mock-component',
-        template: '<div></div>',
-        // styleUrls: ['./course.component.less']
-    })
-    class MockComponent extends ElementRef {
-        constructor() {
-            super('<div class="test"></div>');
-        }
-    }
-
-    let component: MockComponent;
-    let fixture: ComponentFixture<MockComponent>;
+    let component: AboutComponent;
+    let fixture: ComponentFixture<AboutComponent>;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                MockComponent,
-                BorderGreenDirective
-            ]
-        }).compileComponents();
-
-        fixture = TestBed.createComponent(MockComponent);
+        fixture = TestBed.configureTestingModule({
+            declarations: [BorderGreenDirective, AboutComponent],
+            schemas: [NO_ERRORS_SCHEMA]
+        })
+        .createComponent(AboutComponent);
         component = fixture.componentInstance;
-    })
 
-    it('should create component', () => {
-        expect(component).toBeDefined();
     });
 
-    it('should create an instance', () => {
-        const directive = new BorderGreenDirective(new MockComponent());
-        const el = fixture.debugElement.nativeElement.querySelector('div');
-        expect(directive).toBeTruthy();
+    it('should have skyblue border', () => {
+        component.startDate = DATE;
+        fixture.detectChanges();
+        const el = fixture.debugElement.queryAll(By.directive(BorderGreenDirective));
+
+        expect(el[0].nativeElement.style.border).toBe('1px solid skyblue');
+    });
+
+    it('should have greenyellow border', () => {
+        component.startDate = DATE2;
+        fixture.detectChanges();
+        const el = fixture.debugElement.queryAll(By.directive(BorderGreenDirective));
+
+        expect(el[0].nativeElement.style.border).toBe('1px solid greenyellow');
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @Component({
+//     selector: 'app-mock-component',
+//     template: '<div></div>',
+//     styles: ['div { border: 1px solid green; }']
+// })
+// class MockComponent  {
+//     startDate: string;
+
+//     constructor() {
+//     }
+// }
+
+// describe('BorderGreenDirective', () => {
+
+//     let component: MockComponent;
+//     let fixture: ComponentFixture<MockComponent>;
+
+//     beforeEach(async(() => {
+//         TestBed.configureTestingModule({
+//         declarations: [
+//             BorderGreenDirective,
+//             MockComponent
+//         ],
+//         schemas: [NO_ERRORS_SCHEMA]
+//         }).compileComponents().then(() => {
+//             const dir = new BorderGreenDirective()
+//             fixture = TestBed.createComponent(MockComponent);
+//             component = fixture.componentInstance;
+//         });
+//     }));
+
+//     it('should color only p background', () => {
+//         component.startDate = 'Mon Nov 18 2019 12:32:41 GMT+0300';
+//         fixture.detectChanges();
+
+//         const el = fixture.debugElement.queryAll(By.directive(BorderGreenDirective));
+//         console.log(el);
+//     });
+
+    // beforeEach(async(() => {
+    //         TestBed.configureTestingModule({
+    //         declarations: [
+    //             BorderGreenDirective,
+    //             CourseComponent
+    //         ],
+    //         schemas: [NO_ERRORS_SCHEMA]
+    //         }).compileComponents().then(() => {
+    //             fixture = TestBed.createComponent(CourseComponent);
+    //             component = fixture.componentInstance;
+    //         });
+    //     }));
+
+    // it('should color only p background', () => {
+    //     component.item = {
+    //         id: 1,
+    //         title: 'Angular',
+    //         isFavorite: true,
+    //         creationDate: 'Fri Nov 1 2019 12:32:41 GMT+0300',
+    //         duration: 45,
+    //         startDate: 'Mon Nov 18 2019 12:32:41 GMT+0300',
+    //         description: 'test'
+    //     };
+    //     fixture.detectChanges();
+
+    //     const el = fixture.debugElement.queryAll(By.directive(BorderGreenDirective));
+    //     console.log(el[0].classes);
+    // });
+// });
