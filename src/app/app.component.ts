c673d8d539b2b64f6ad05a4c8602cee7821f7aac
maxim dashkov <insektary@mail.ru>
2019-12-02 22:55:01 +0300

    implements breadcrumbs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnChanges} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 import {AuthService} from './auth.service';
 
 @Component({
@@ -7,14 +9,20 @@ import {AuthService} from './auth.service';
     styleUrls: ['./app.component.less'],
     providers: [AuthService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnChanges {
     title = 'angular-mentoring';
+    private subscription: Subscription;
 
-    constructor() {
-
+    constructor(private activateRoute: ActivatedRoute) {
+        // this.subscription = activateRoute.url.subscribe(url => console.log(url));
+        // console.log(activateRoute.snapshot.paramMap.get('id'))
     }
 
     ngOnInit() {
 
+    }
+
+    ngOnChanges() {
+        // console.log(this.activateRoute.snapshot)
     }
 }
--- src/app/breadcrumbs/breadcrumbs.component.ts
+++ src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-
+import {ActivatedRoute, Router} from '@angular/router';
+import {Location} from '@angular/common';
 @Component({
     selector: 'app-breadcrumbs',
     templateUrl: './breadcrumbs.component.html',
@@ -8,17 +9,27 @@ import {Component, OnInit} from '@angular/core';
 export class BreadcrumbsComponent implements OnInit {
 
     public breadcrumbs: any[];
+    public currentUrl: string;
 
-    constructor() {
-        this.breadcrumbs = [
-            {
-                id: 1,
-                title: 'Courses'
-            }
-        ];
+    constructor(private route: ActivatedRoute, private router: Router, public location: Location) {
+
+        this.currentUrl = location.path();
+        this.breadcrumbs = this.currentUrl.split('/').filter(Boolean);
+        router.events.subscribe(() => this.onRouteChange());
     }
 
     ngOnInit() {
+
+    }
+
+    onRouteChange() {
+        const {currentUrl, location} = this;
+        const newUrl = location.path();
+
+        if (currentUrl !== newUrl) {
+            this.currentUrl = newUrl;
+            this.breadcrumbs = this.currentUrl.split('/').filter(Boolean);
+        }
     }
 
 }
