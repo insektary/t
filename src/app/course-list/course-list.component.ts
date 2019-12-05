cfd9feda5622823d3c05852b0e47da59bbfc8b6b
maxim dashkov <insektary@mail.ru>
2019-12-05 22:44:19 +0300

    implements actions with courses

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {Course} from 'src/app/interfaces/course';
 import {CoursesService} from '../courses.service';
 
 @Component({
